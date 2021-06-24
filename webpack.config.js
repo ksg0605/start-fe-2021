7291edc733d493f0e87176c124bfa2194cf0df4d
sunkyu Kim <yyh2337@naver.com>
2021-06-24 16:48:11 +0900

    webpack setting

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,7 @@
-const path = require("path");
-
 module.exports = {
   entry: "./src/app.js",
   output: {
     filename: "result.js",
-    path: path.resolve(__dirname, "dist"),
+    path: `${__dirname}/dist`,
   },
 };
